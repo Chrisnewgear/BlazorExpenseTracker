7c5747424fec72200cdd719131a8ab037ff604b3
Christian Abraham Sánchez Sánchez <chrisnewgear2011@gmail.com>
2023-08-01 17:43:25 -0500

    PRIMER COMMIT DESDE LOCAL

---
+++ BlazorExpenseTracker.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using BlazorExpenseTracker.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpenseTracker.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private SqlConfiguration _connectionString;
+
+        public CategoryRepository(SqlConfiguration connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected SqlConnection dbConnection()
+        {
+            return new SqlConnection(_connectionString.ConnectionString);
+        }
+
+        public Task<bool> DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryDetaails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorExpenseTracker.Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpenseTracker.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Category Name is Required")]
+
+        public string Name { get; set; }
+    }
+}
